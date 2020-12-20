7f1ecc6b90ea3b90bbedbba9e3c4960d9b679a2b
Raskovan <alexey.katalkin@gmail.com>
2020-12-20 17:43:40 -0500

    revert strategy

--- src/service-worker.js
+++ src/service-worker.js
@@ -25,26 +25,26 @@ precacheAndRoute(self.__WB_MANIFEST)
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$')
-// registerRoute(
-//   // Return false to exempt requests from being fulfilled by index.html.
-//   ({ request, url }) => {
-//     // If this isn't a navigation, skip.
-//     if (request.mode !== 'navigate') {
-//       return false
-//     } // If this is a URL that starts with /_, skip.
+registerRoute(
+  // Return false to exempt requests from being fulfilled by index.html.
+  ({ request, url }) => {
+    // If this isn't a navigation, skip.
+    if (request.mode !== 'navigate') {
+      return false
+    } // If this is a URL that starts with /_, skip.
 
-//     if (url.pathname.startsWith('/_')) {
-//       return false
-//     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+    if (url.pathname.startsWith('/_')) {
+      return false
+    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
-//     if (url.pathname.match(fileExtensionRegexp)) {
-//       return false
-//     } // Return true to signal that we want to use the handler.
+    if (url.pathname.match(fileExtensionRegexp)) {
+      return false
+    } // Return true to signal that we want to use the handler.
 
-//     return true
-//   },
-//   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
-// )
+    return true
+  },
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+)
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
@@ -60,11 +60,6 @@ registerRoute(
       new ExpirationPlugin({ maxEntries: 50 })
     ]
   })
-)
-
-registerRoute(
-  ({ url }) => url.pathname.startsWith('/'),
-  new StaleWhileRevalidate()
 )
 
 // This allows the web app to trigger skipWaiting via
